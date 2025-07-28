6f8dd3052f5f5cd5bbe9eaaa9af9480c28056014
Lihn <lihn1608@gmail.com>
2025-07-28 13:25:24 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/log-test', function (Request $request) {
+    Log::info('Log test from Postman', [
+        'body' => $request->all(),
+    ]);
+    return response()->json(['message' => 'Log saved']);
+});
